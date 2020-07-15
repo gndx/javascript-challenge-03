c93de7ab98ca7e84af68e52f4da05ac9a6317173
Axel Espinosa <axeldavid45@gmail.com>
2020-07-15 18:16:57 -0500

    Code refactor using async await

--- src/index.js
+++ src/index.js
@@ -2,37 +2,40 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (api) => {
+    try {
+        const request = await fetch(api);
+        const responseJson = await request.json();
+        const characters = await responseJson.results;
+
+        let renderCharacters = await characters.map(character => {
+            return `
+              <article class="Card">
+                <img src="${character.image}" />
+                <h2>${character.name}<span>${character.species}</span></h2>
+              </article>
+      `;}).join('');
+
+        let newItem = document.createElement('section');
+        newItem.classList.add('Items');
+        newItem.innerHTML = renderCharacters;
+        $app.appendChild(newItem);
+
+    } catch (err) {
+        console.error(err.message);
+    }
 }
 
 const loadData = () => {
-  getData(API);
+    getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
+    if (entries[0].isIntersecting) {
+        loadData();
+    }
 }, {
-  rootMargin: '0px 0px 100% 0px',
+    rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
