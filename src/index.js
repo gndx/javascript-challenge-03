373b32adb2b693694162a7c281d32ca5ce83b40e
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-08-10 15:33:38 -0500

    Solución reto js #3

--- src/index.js
+++ src/index.js
@@ -1,38 +1,51 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
+const NEXT_KEY = "next_fetch";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
+window.onbeforeunload = function () {
+  localStorage.removeItem(NEXT_KEY);
+};
+
+const getData = async (api) => {
+  try {
+    let request = await fetch(api);
+    let response = await request.json();
+    const characters = response.results;
+    let output = await characters
+      .map((character) => {
         return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+    `;
+      })
+      .join("");
+    let newItem = document.createElement("section");
+    newItem.classList.add("Items");
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+    localStorage.setItem(NEXT_KEY, response.info.next);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const loadData = () => {
-  getData(API);
-}
+  const from = localStorage.getItem(NEXT_KEY) || API;
+  getData(from);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+);
 
 intersectionObserver.observe($observe);
