35431171a2edd042b019d81e185715f69eff54f6
AbejaCruz <dacruzt@outlook.com>
2020-10-04 11:16:45 -0500

    solucion de problemas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
-
+let API = 'https://rickandmortyapi.com/api/character';
+localStorage.clear();
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      localStorage.setItem("next_fetch",response.info.next)
       const characters = response.results;
       let output = characters.map(character => {
         return `
@@ -23,13 +24,25 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async(API) => {
+  await getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    loadData();
+    
+    if(localStorage.getItem("next_fetch") != null ){
+      API=localStorage.getItem("next_fetch")
+      loadData(API);
+    }
+    else if( localStorage.getItem("next_fetch") == "") {
+      let newItem = document.createElement('h1');
+      newItem.innerHTML = 'Ya no hay personajes';
+      $app.appendChild(newItem);
+      intersectionObserver.disconnect()
+    }else{
+      loadData(API);
+    }
   }
 }, {
   rootMargin: '0px 0px 100% 0px',
